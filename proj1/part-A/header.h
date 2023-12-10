8bdc09ef920e484edc0081ceb6f5132f3b4b2723
nuno.j.santos <nunosantos2001@hotmail.com>
2023-12-10 16:14:02 +0000

    part A donegit add *!

---
+++ proj1/part-A/header.h
@@ -0,0 +1,71 @@
+#define WINDOW_SIZE 15
+#include <stdbool.h>
+
+// declaration the struct corresponding to the exchanged messages
+
+typedef enum direction_t {UP, DOWN, LEFT, RIGHT} direction_t;
+
+typedef struct client_message {
+
+   int msg_type;
+
+   int code;
+
+   char ch;
+
+   int n_roaches;
+
+   int r_scores[10];
+
+   direction_t direction;
+
+   direction_t r_direction[10];
+
+   int r_bool[10];
+} client_message;
+
+typedef struct remote_screen {
+
+   int msg_type;
+
+   char ch;
+
+   int old_x, old_y, new_x, new_y;
+
+   int score;
+
+   direction_t old_direction, new_direction;
+
+   int screen_roaches[10][4];
+
+} remote_screen;
+
+typedef struct response_to_client {
+
+   int status;
+   
+   int code;
+
+   int assigned_char;
+
+   int score;
+
+} response_to_client;
+
+typedef struct lizards_struct {
+
+   char ch;
+
+   int code;
+
+   int x;
+
+   int y;
+
+   int score;
+
+   bool winner;
+
+   direction_t direction;
+
+} lizards_struct;
